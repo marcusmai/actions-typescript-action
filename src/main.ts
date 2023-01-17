cb3d7727ab87eac09f9cae153a6cde47157f5af5
marcusmai <marcus.mai@me.com>
2023-01-17 10:45:53 +0100

    Update

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@
 import * as core from '@actions/core'
 
 async function run(): Promise<void> {
-  const environment = core.getInput('environment')
+  //const environment = core.getInput('environment')
   //console.log(`The input is: ${environment}`)
 }
 
